679023c7c71306f37338be23472103948356e091
Magnus Helmersson <magnus.helmersson@gmail.com>
2016-09-17 13:00:11 +0200

    Animate the skilldots

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Education = () => (
   <section>
     Some experience...
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/skilldots.scss'
 
-const SkillDot = ({ score, scoreLimit }) => (
-  <div className="skilldots">
-    { Array(scoreLimit).fill().map((_, idx) => (
-      idx < score ? (
-        <span key={idx} className="skilldot skilldot-active"></span> 
-      ) : (
-        <span key={idx} className="skilldot skilldot-inactive"></span>
-      )
-    ) ) }
-  </div>
-)
+class SkillDots extends Component {
+  constructor () {
+    super()
+    this.tick = this.tick.bind(this)
+    this.state = { value: 0 }
+  }
+  tick() {
+    if (this.state.value < this.props.score ) {
+      this.setState({ value: this.state.value + 1})
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.score !== this.props.score) {
+      this.setState({ value: 0 })
+    }
+  }
+  componentDidUpdate() {
+    if (this.state.value < this.props.score ) {
+      setTimeout(this.tick, 50);
+    }
+  }
+  componentDidMount() {
+    setTimeout(this.tick, 50);
+  }
+  render () {
+    const { score, scoreLimit } = this.props
+    return (
+      <div className="skilldots">
+        { Array(scoreLimit).fill().map((_, idx) => (
+          idx < this.state.value ? (
+            <span key={idx} className="skilldot skilldot-active"></span>
+          ) : (
+            <span key={idx} className="skilldot skilldot-inactive"></span>
+          )
+        ) ) }
+      </div>
+    )
+  }
+}
 
 const Skills = ({ skills, scoreLimit }) => (
   <section>
@@ -18,7 +47,7 @@ const Skills = ({ skills, scoreLimit }) => (
     { skills.sort((a, b) => a.score - b.score).map(skill => (
       <li key={skill.name} className="skills">
         {skill.name}:
-        <SkillDot score={skill.score} scoreLimit={scoreLimit} />
+        <SkillDots score={skill.score} scoreLimit={scoreLimit} />
       </li>
     ) ) }
     </ul>
